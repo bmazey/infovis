680ca665af68061a6dc1463f7592936f88b941f7
Brandon <b.mazey@nyu.edu>
2019-10-04 11:13:00 -0400

    ok - using flask render_template() to serve index.html

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+import os
+import sys
+from flask import Flask, render_template
+
+sys.path.append(os.path.dirname(__name__))
 app = Flask(__name__)
 
 
 @app.route('/')
-def root():
-    return app.send_static_file('index.html')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
